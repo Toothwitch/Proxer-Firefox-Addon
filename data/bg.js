c516ea834143e9519852f434574fba35048ddce1
Toothwitch <witch@toothwit.ch>
2015-05-31 20:47:40 +0200

    Finished adding logic
    Ported all existing functions from the Chrome extensions over to
    Firefox.
    Code cleaning will follow soon...

--- data/bg.js
+++ data/bg.js
@@ -1,15 +1,31 @@
-self.port.on("test", function(info) {
-  document.getElementById("content").innerHTML += info;
+self.port.on("content", function(info) {
+  document.getElementById("content").innerHTML = info;
 });
 
 
-
-function doLogin(){
-	Request({
-	url: "https://proxer.me/login?format=json&action=login",
-	content: { username: 'Toothwitch', password : 'Zumka22wrew45tz' },
-	onComplete: function (response) {
-		console.log( response.text );
+document.onkeydown = function(e){
+	if(e.keyCode == 13){
+			var username = document.getElementById("username").value;
+			var pass = document.getElementById("pass").value;
+			document.getElementById("content").innerHTML = "Processing...";
+			var xhr = new XMLHttpRequest();
+			xhr.open("POST", "https://proxer.me/login?format=json&action=login", true);
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == 4) {
+					// JSON.parse does not evaluate the attacker's scripts.
+					var resp = JSON.parse(xhr.responseText);
+					if(resp.error == 0){
+						document.getElementById("content").innerHTML = "Willkommen "+username.toString()+"!";
+						self.port.emit("loggedIn", "");
+					}else{
+						document.getElementById("content").innerHTML = "Fehler";
+					}
+				}
+			}
+			var data = new FormData();
+			data.append('username', username);
+			data.append('password', pass);
+			xhr.send(data);
+	}else{
 	}
-	}).post();
 }
